7e694c51c333c643c5fbe97735a6137418192ece
AdomBoateng <craxybaboon6@gmail.com>
2023-11-24 13:39:58 +0000

    A loop to generate a series of data with a 'number' field ranging from 0 to 999, and then sending that data to a Kafka topic using a producer

---
+++ main.py
@@ -0,0 +1,15 @@
+#IMPORTS
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+
+#Name of topic and producer code
+topic_name ='foodhub'
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda x: dumps(x).encode('utf-8'))
+
+#content to produce
+for e in range(1000):
+    data = {'number':e}
+    print(data)
+    producer.send(topic_name,value=data)
+    sleep(5)
